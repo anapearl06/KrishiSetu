ba9b1911558c4b5701e872c17afb8e421c9d4ad4
Raaj Giri <raj.ash2493@gmail.com>
2026-08-27 23:40:22 +0530

    integrating neon for postgres

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,43 +1,21 @@
 package config
 
-import (
-	"os"
-	"strconv"
-	"time"
-)
+import "os"
 
 type Config struct {
-	AppEnv        string
-	AppPort       string
-	DBHost        string
-	DBPort        string
-	DBUser        string
-	DBPassword    string
-	DBName        string
-	DBSSLMode     string
-	JWTSecret     string
-	JWTExpiration time.Duration
+	AppEnv             string
+	AppPort            string
+	DatabaseURL        string
+	JWTSecret          string
+	JWTExpirationHours string
 }
 
 func Load() Config {
-	expirationHours, err := strconv.Atoi(
-		os.Getenv("JWT_EXPIRATION_HOURS"),
-	)
-
-	if err != nil || expirationHours <= 0 {
-		expirationHours = 24
-	}
-
 	return Config{
-		AppEnv:        os.Getenv("APP_ENV"),
-		AppPort:       os.Getenv("APP_PORT"),
-		DBHost:        os.Getenv("DB_HOST"),
-		DBPort:        os.Getenv("DB_PORT"),
-		DBUser:        os.Getenv("DB_USER"),
-		DBPassword:    os.Getenv("DB_PASSWORD"),
-		DBName:        os.Getenv("DB_NAME"),
-		DBSSLMode:     os.Getenv("DB_SSLMODE"),
-		JWTSecret:     os.Getenv("JWT_SECRET"),
-		JWTExpiration: time.Duration(expirationHours) * time.Hour,
+		AppEnv:             os.Getenv("APP_ENV"),
+		AppPort:            os.Getenv("APP_PORT"),
+		DatabaseURL:        os.Getenv("DATABASE_URL"),
+		JWTSecret:          os.Getenv("JWT_SECRET"),
+		JWTExpirationHours: os.Getenv("JWT_EXPIRATION_HOURS"),
 	}
 }
